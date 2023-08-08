07a53f2990f8e0ff2855686d46f23b3663317f63
Joshua Murray <115622052+JoshMurray99@users.noreply.github.com>
2023-08-08 15:04:47 +0100

    added AI  itinerary routes

--- server/application/routes/albumRoutes.py
+++ server/application/routes/albumRoutes.py
@@ -2,4 +2,4 @@ from application import app, db
 from flask import Blueprint
 from application.controllers.albumController import *
 
-user_routes = Blueprint("album_routes", __name__)
+album_routes = Blueprint("album_routes", __name__)
