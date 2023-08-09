ec10a4ac84cbbb6758fe4bdc6d19bae5414e6321
ioptyu2 <mbatta1996@hotmail.com>
2023-08-09 17:48:18 +0100

    db changed and content route

--- server/application/controllers/contentController.py
+++ server/application/controllers/contentController.py
@@ -17,7 +17,7 @@ def create(id):
     album_id = id
     if not description:
         return jsonify({"error": "Missing parameters"}), 400
-    content = Content(photo = photo, description = description, tags = tags, album_id = id)
+    content = Content(photo = photo, description = description, tags = tags, album_id = album_id)
     db.session.add(content)
     db.session.commit()
     return jsonify({"Message": "Successfully added new content"}), 201
@@ -35,10 +35,24 @@ def index_content(id):
     return content, 200
 
 def update_content(id):
-    
-    return
-
+    data = request.get_json()
+    content = db.session.get(Content, id)
+    photo = data.photo or content.photo
+    description = data.description or content.description
+    tags = data.tags or content.tags
+    db.session.commit()
+    return jsonify({"message": "Content details updated!"}), 200
 
 def destroy_content(id):
+    content = db.session(Content, id)
+    db.session.delete(content)
+    db.session.commit()
+    return jsonify({'message': 'Content deleted!'}), 204
 
-    return
+def destroy_content_by_album(id):
+    contents = index_content_by_album(id)
+    for c in contents:
+        content = db.session.get(Content, c.get("content_id"))
+        db.session.delete(content)
+    db.session.commit()
+    return jsonify({'message': 'Content deleted!'}), 204
