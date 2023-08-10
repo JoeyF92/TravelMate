d1598971b40f8f7e4951f150abf207c2ef72652e
JoeyF92 <jf-9294@protonmail.com>
2023-08-10 09:12:59 +0100

    added groups page, and join and create form components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import {
-  Landing
-} from "./pages"
+import * as Pages from "./pages";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-    </Routes>
-   
+      <Routes>
+        <Route path="/" element={<Pages.Landing />} />
+        <Route path="/groups" element={<Pages.GroupsPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
