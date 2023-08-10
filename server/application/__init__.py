4235604a7e3e3561bbff37d18b2a2b7d87807ece
ioptyu2 <mbatta1996@hotmail.com>
2023-08-10 14:56:35 +0100

    uncommented blueprint

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -17,9 +17,9 @@ db = SQLAlchemy(app)
 
 from application.routes import *
 app.register_blueprint(user_routes, url_prefix="/user")
-# app.register_blueprint(album_routes, url_prefix="/album")
+app.register_blueprint(album_routes, url_prefix="/album")
 # app.register_blueprint(bucket_routes, url_prefix="/bucket")
-# app.register_blueprint(content_routes, url_prefix="/content")
+app.register_blueprint(content_routes, url_prefix="/content")
 app.register_blueprint(itinerary_routes, url_prefix="/itinerary")
 # app.register_blueprint(packing_routes, url_prefix="/packing")
 # app.register_blueprint(preference_routes, url_prefix="/preference")
