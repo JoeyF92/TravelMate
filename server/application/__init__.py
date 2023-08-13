aaef7c36f6181646f6b73dccb36a129bd36a6d24
mahirjali <mahirjalil@hotmail.com>
2023-08-13 16:08:09 +0100

    updates for deployment

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from dotenv import load_dotenv
 import os
 import bcrypt
 
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
