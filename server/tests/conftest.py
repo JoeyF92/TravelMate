6e07215871af1b3afb81a7e368b4f1ef60e77d2e
ioptyu2 <mbatta1996@hotmail.com>
2023-08-15 15:05:12 +0100

    db fixes

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,10 +1,6 @@
 from application import create_app
 import pytest, os
 
-# @pytest.fixture
-# def create_app():
-#     return create_app("TEST")
-
 @pytest.fixture()
 def client():
     app = create_app("TEST")
